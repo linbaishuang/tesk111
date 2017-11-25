14cc656c049faca735951d1aae0d6a3e8371568a
linbaishuang <1765259718@qq.com>
2017-11-25 14:46:41 +0800

    完成

--- main/main.js
+++ main/main.js
@@ -1,12 +1,22 @@
 function main(num) {
-    var str1 = "${num} + bottles of beer on the wall, 99 bottles of beer.\n" +
-            "Take one down and pass it around, 98 bottles of beer on the wall.\n" +
-        "${num-1} + bottles of beer on the wall, 98 bottles of beer.\n" +
-            "Take one down and pass it around, 97 bottles of beer on the wall.\n";
-    // var str1 = "aaa";
-    if(num > 1){
-        return str1;
+    var str1 = "";
+    while(num > 2){
+        str1 +=  num + " bottles of beer on the wall, " + num + " bottles of beer.\n" +
+            "   Take one down and pass it around, " + (num-1) + " bottles of beer on the wall.\n" ;
+        num--;
     }
+    if(num == 2){
+        str1 +=  num + " bottles of beer on the wall, " + num + " bottles of beer.\n" +
+            "   Take one down and pass it around, " + (num-1) + " bottle of beer on the wall.\n" ;
+        num--;
+    }
+    if(num == 1){
+        str1 += num + " bottle of beer on the wall, " + num + " bottle of beer.\n"+
+            "   Take one down and pass it around, no more bottles of beer on the wall.\n"+
+            "   No more bottles of beer on the wall, no more bottles of beer.\n"+
+            "   Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    }
+    return str1;
 }
 
 module.exports = main;
